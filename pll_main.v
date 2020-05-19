959db06a92831c01bc972f9cb7439094dd2e616d
Maximenkov <FapDayz@gmail.com>
2020-05-19 13:44:58 +0300

    Fixed video_spi

--- pll_main.v
+++ pll_main.v
@@ -110,9 +110,9 @@ module pll_main (
 		altpll_component.clk1_duty_cycle = 50,
 		altpll_component.clk1_multiply_by = 1,
 		altpll_component.clk1_phase_shift = "0",
-		altpll_component.clk2_divide_by = 25,
+		altpll_component.clk2_divide_by = 4,
 		altpll_component.clk2_duty_cycle = 50,
-		altpll_component.clk2_multiply_by = 28,
+		altpll_component.clk2_multiply_by = 1,
 		altpll_component.clk2_phase_shift = "0",
 		altpll_component.compensate_clock = "CLK0",
 		altpll_component.inclk0_input_frequency = 10000,
@@ -195,7 +195,7 @@ endmodule
 // Retrieval info: PRIVATE: DUTY_CYCLE2 STRING "50.00000000"
 // Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "32.000000"
 // Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE1 STRING "4.000000"
-// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE2 STRING "112.000000"
+// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE2 STRING "25.000000"
 // Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
 // Retrieval info: PRIVATE: EXT_FEEDBACK_RADIO STRING "0"
 // Retrieval info: PRIVATE: GLOCKED_COUNTER_EDIT_CHANGED STRING "1"
@@ -228,7 +228,7 @@ endmodule
 // Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "32.00000000"
 // Retrieval info: PRIVATE: OUTPUT_FREQ1 STRING "4.00000000"
-// Retrieval info: PRIVATE: OUTPUT_FREQ2 STRING "112.00000000"
+// Retrieval info: PRIVATE: OUTPUT_FREQ2 STRING "25.00000000"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_MODE1 STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_MODE2 STRING "1"
@@ -289,9 +289,9 @@ endmodule
 // Retrieval info: CONSTANT: CLK1_DUTY_CYCLE NUMERIC "50"
 // Retrieval info: CONSTANT: CLK1_MULTIPLY_BY NUMERIC "1"
 // Retrieval info: CONSTANT: CLK1_PHASE_SHIFT STRING "0"
-// Retrieval info: CONSTANT: CLK2_DIVIDE_BY NUMERIC "25"
+// Retrieval info: CONSTANT: CLK2_DIVIDE_BY NUMERIC "4"
 // Retrieval info: CONSTANT: CLK2_DUTY_CYCLE NUMERIC "50"
-// Retrieval info: CONSTANT: CLK2_MULTIPLY_BY NUMERIC "28"
+// Retrieval info: CONSTANT: CLK2_MULTIPLY_BY NUMERIC "1"
 // Retrieval info: CONSTANT: CLK2_PHASE_SHIFT STRING "0"
 // Retrieval info: CONSTANT: COMPENSATE_CLOCK STRING "CLK0"
 // Retrieval info: CONSTANT: INCLK0_INPUT_FREQUENCY NUMERIC "10000"
--- video_spi.v
+++ video_spi.v
@@ -16,13 +16,15 @@ spi_master_reg #(
   .CPOL (1),
   .CPHA (0),
   .WIDTH (24),
-  .PAUSE (3),
+  .PAUSE (3), // actual pause will be +1
   .BIDIR (1),
-  .SWAP_DIR_BIT_NUM (8)
+  .SWAP_DIR_BIT_NUM (8),
+  .SCLK_CONST (1)
 )
 spi_master_reg (
   .n_rst (n_rst),
-  .sclk (sclk_full),
+  .sys_clk (sclk_full),
+  .sclk (sckv_fpga),
   .n_cs (slv_fpga),
   .sdio (sdatav_fpga),
   .in_data ({1'b1,23'b0}),
@@ -31,6 +33,8 @@ spi_master_reg (
   .miso_reg (miso_reg),
   .miso_reg_ena (miso_reg_ena)
 );
+
+
 
 wire [23:0] miso_reg;
 wire miso_reg_ena;
@@ -41,9 +45,6 @@ always@(posedge sclk_full or negedge n_rst)
   else
     if(miso_reg_ena)
       parall_data <= miso_reg[11:0];
-
-
-assign sckv_fpga = slv_fpga ? 1'b1 : sclk_full; // 1'b1 is CPOL
 
 
 
